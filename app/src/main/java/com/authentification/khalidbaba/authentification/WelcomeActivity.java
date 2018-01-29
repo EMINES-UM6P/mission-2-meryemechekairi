e5cef0c10baba2d1b6e159f1e2903fbc425f9144
meryeme.chekairi <meryeme.chekairi@emines.um6p.ma>
2018-01-29 18:25:49 +0000

    Done

--- app/src/main/java/com/authentification/khalidbaba/authentification/WelcomeActivity.java
+++ app/src/main/java/com/authentification/khalidbaba/authentification/WelcomeActivity.java
@@ -1,14 +1,19 @@
 package com.authentification.khalidbaba.authentification;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+import java.time.Instant;
 
 /**
  * Created by khalidbaba on 28/01/2018.
  */
 
-public class WolcomeActivity extends Activity {
+public class WelcomeActivity extends Activity {
 
     //TODO : Declare all variables (TextView, String)
 
@@ -18,7 +23,14 @@ public class WolcomeActivity extends Activity {
         setContentView(R.layout.activity_welcome);
 
         //TODO : Instantiate all objects
-
+        TextView email;
+        String _email;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Intent i =getIntent();
+        _email = i.getStringExtra("email");
+        email = (TextView) findViewById(R.id.email);
+        email.setText(_email);
 
         //TODO  : Get the intent content and show the Email in the TextView
     }
